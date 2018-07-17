592d01569d0b7886d5728d01ee83af11b010a679
Codrin Kruijne <codrin.kruijne@gmail.com>
2018-07-17 22:12:41 +0200

    Created several panes for index information, made world map clickable.

--- liveso/server.R
+++ liveso/server.R
@@ -2,32 +2,42 @@ library(shiny)
 library(xlsx)
 library(dplyr)
 library(plotly)
-library(maps)
-library(leaflet)
 library(countrycode)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   
-  # # Leaflet map
-  # output$worldmap <- renderLeaflet({
-  #   library(maps)
-  #   mapCountries = map("world", fill = TRUE, plot = FALSE)
-  #   leaflet(data = mapCountries,
-  #           options = leafletOptions(noWrap = TRUE)) %>%
-  #     addTiles() %>%
-  #     addPolygons(fillColor = topo.colors(10, alpha = NULL), stroke = FALSE)
-  # })
+  # HPI data
+  HPI <- read.xlsx("data/hpi-data-2016.xlsx",
+                   sheetName = "Complete HPI data",
+                   rowIndex = 6:146,
+                   header = TRUE,
+                   colIndex = 2:15,
+                   stringsAsFactors = FALSE)
+  HPI[2:3] <- lapply(HPI[2:3], as.factor)
   
-  # Plotly output
-  output$plotly_map <- renderPlotly({
-    # HPI data
-    df <- read.xlsx("data/hpi-data-2016.xlsx",
-                    sheetName = "Complete HPI data",
-                    rowIndex = 6:146,
-                    header = TRUE,
-                    colIndex = 2:15)
-    df$iso3 <- countrycode(df$Country, "country.name", "iso3c")
+  # SPI data
+  SPI <- read.xlsx("data/SPI 2017 Results.xlsx",
+                   sheetName = "2017 SPI",
+                   rowIndex = 1:273,
+                   header = TRUE,
+                   colIndex = 1:76,
+                   stringsAsFactors = FALSE)
+  SPI[3:76] <- lapply(SPI[3:76], as.numeric)
+  SPI[1:2] <- lapply(SPI[1:2], as.factor)
+  
+  #  merge country data
+  
+  df <- SPI[c("Country", "Country.Code", "Social.Progress.Index")]
+  df_merged <- df %>% left_join(HPI[c("Country", "Happy.Planet.Index")], by = "Country")
+  
+  # Create hover text
+  df_merged$hover <- with(df_merged, paste(Country, '<br>',
+                                           "HPI", round(Happy.Planet.Index, 2), '<br>',
+                                           "SPI", round(Social.Progress.Index, 2)))
+  
+  # World map selector
+  output$world_map <- renderPlotly({
     
     # light grey boundaries
     l <- list(color = toRGB("grey"), width = 0.5)
@@ -39,13 +49,54 @@ shinyServer(function(input, output) {
       projection = list(type = "winkel triple")
     )
     
-    p <- plot_geo(data = df,
+    p <- plot_geo(data = df_merged,
                   z = ~Happy.Planet.Index,
                   color = ~Happy.Planet.Index,
-                  text = ~Country,
-                  locations = ~iso3,
+                  text = ~hover,
+                  locations = ~Country.Code,
                   type = 'choropleth') %>%
             layout(geo = g)
   })
   
+  # Country map
+  output$country_map <- renderPlotly({
+    d <- event_data("plotly_click")
+    # light grey boundaries
+    l <- list(color = toRGB("grey"), width = 0.5)
+    
+    # specify map projection/options
+    g <- list(
+      scope = "usa",
+      showframe = FALSE,
+      showcoastlines = FALSE,
+      projection = list(type = "winkel triple")
+    )
+    
+    p <- plot_geo(data = df_merged,
+                  type = 'choropleth') %>%
+      layout(geo = g)
+  })
+  
+  # Testing output click
+  
+  output$country <- renderPrint({
+    d <- event_data("plotly_click")
+    # print(str(d))
+    # print(d)
+    df_points <- df_merged %>% filter(!is.na(Happy.Planet.Index) & !is.na(Social.Progress.Index)) %>% arrange(Country)
+    if (is.null(d)) "Click on a country to view data" else df_points[d$pointNumber + 1,]$Country
+  })
+ 
+  output$SPI <- renderPrint({
+    d <- event_data("plotly_click")
+    df_points <- df_merged %>% filter(!is.na(Happy.Planet.Index) & !is.na(Social.Progress.Index)) %>% arrange(Country)
+    if (is.null(d)) "Click on a country to view data" else df_points[d$pointNumber + 1,]$Social.Progress.Index
+  })
+   
+  output$HPI <- renderPrint({
+    d <- event_data("plotly_click")
+    df_points <- df_merged %>% filter(!is.na(Happy.Planet.Index) & !is.na(Social.Progress.Index)) %>% arrange(Country)
+    if (is.null(d)) "Click on a country to view data" else df_points[d$pointNumber + 1,]$Happy.Planet.Index
+  })
+
 })
--- liveso/ui.R
+++ liveso/ui.R
@@ -1,15 +1,59 @@
 library(shiny)
 library(plotly)
-library(leaflet)
 
 # Define UI for application that draws a histogram
 shinyUI(
   fluidPage(
-    # Show a world map
     fluidRow(
-      h1("World Map"),
-      # leafletOutput("worldmap"),
-      plotlyOutput("plotly_map")
+      h1("Towards a global Natural Progress Index")
+    ),
+    fluidRow( # Selector map
+      column(8,
+        plotlyOutput("world_map")       
+      ),
+      column(4,
+        verbatimTextOutput("country"),
+        plotlyOutput("country_map")
+      )
+    ),
+    fluidRow( # Country Natural Progress Indicator
+      h2("How does this country measure up to Life's Principles?")
+    ),
+    fluidRow(
+      column(2,
+        h3("Evolve to Survive")
+      ),
+      column(2,
+        h3("Evolve to Survive")
+      ),
+      column(2,
+        h3("Evolve to Survive")       
+      ),
+      column(2,
+        h3("Evolve to Survive")
+      ),
+      column(2,
+        h3("Evolve to Survive")
+      ),
+      column(2,
+        h3("Evolve to Survive")
+      )
+    ),
+    fluidRow(
+      h2("How does this company compare on previous indicators?")
+    ),
+    fluidRow( # Comparison Indicators
+      column(4, # GDP Column
+        h3("GDP")
+      ),
+      column(4, # SPI Column
+        h3("Social Progress Index"),
+        verbatimTextOutput("SPI")
+      ),
+      column(4, # HPI Column
+        h3("Happy Planet Index"),
+        verbatimTextOutput("HPI")
+      )
     )
   )
 )
